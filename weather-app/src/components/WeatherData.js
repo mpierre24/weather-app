af2b52765f5b3dea53537f3a1ff8608bcaa5079a
Marlon Pierre <marlonpierre@Marlons-MBP.frontier.com>
2023-05-31 20:26:38 -0400

    Temperature change from celsius to fahrenheit

--- weather-app/src/components/WeatherData.js
+++ weather-app/src/components/WeatherData.js
@@ -4,7 +4,7 @@ function WeatherData({ weatherData }) {
   return (
     <div>
       <h2>{weatherData.name}</h2>
-      <p>Temperature: {Math.round(weatherData.main.temp)}°C</p>
+      <p>Temperature: {Math.round(weatherData.main.temp)}°F</p>
       <p>Weather: {weatherData.weather[0].description}</p>
     </div>
   );
